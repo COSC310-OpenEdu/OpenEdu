1c1ff6b2188f8d408749b9cf30bf3aceb6524df5
SilverSix6 <aidanmorris810@gmail.com>
2024-03-15 18:37:04 -0700

    Changed test names to better suite the tests

--- tests/test_UsernamePasswordCheck.py
+++ tests/test_UsernamePasswordCheck.py
@@ -1,14 +1,14 @@
 from src.Database.Check.UsernamePasswordCheck import UsernamePasswordCheck
 import pytest
 
-def test_UsernamePasswordCheck():
+def test_UsernamePasswordCheck_valid():
     username = "jsmith"
     password = "jsmith1234"
 
     validLogin = UsernamePasswordCheck.check((username,password))
     assert(validLogin == True)
     
-def test_UsernamePasswordCheck():
+def test_UsernamePasswordCheck_invalid():
     username = "jsmith"
     password = "wrongpassword"
 
