9b74179b6572ddb536c9216514bc1039ad299e25
Joy Musiel <joymusiel@gmail.com>
2024-03-19 17:45:45 -0700

    replaced button.js with button.css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session, jsonify
 
 from src.User import User
 from src.Database.DatabaseManager import DatabaseManager
@@ -97,27 +97,35 @@ def seeGrades():
 
 
 @app.route("/teacher/COSC310/assignments/createQuiz", methods = ['POST', 'GET'])
-def createAssignment():
-   if request.method == 'GET':
-       return render_template("teacher/createQuiz.html")
+def createQuiz():
+    if request.method == 'GET':
+        return render_template("teacher/createQuiz.html")
+    if request.method == 'POST':
+        questionForm = request.form
+        return render_template("teacher/publishQuiz.html", questionForm=questionForm)
 
-@app.route("/teacher/dashboard", methods = ['POST','GET'])
-def teacherDash():
+@app.route("/teacher/homepage", methods = ['POST','GET'])
+def teacherHome():
     # temporarily getting a list of all courses
     courses = []
     for i in range(1,5):
         courseId = i
         courseName = SelectCourseQuery.query((courseId,))
         courses.append(courseName[0])
-    return render_template("teacher/dashboard.html", courses = courses)
+    return render_template("teacher/homepage.html", courses = courses)
 
-@app.route("/teacher/COSC310/dashboard", methods = ['POST','GET'])
+@app.route("/teacher/COSC310/dashboard")
 def teacherCourseDash():
     return render_template("teacher/courseDashboard.html")
 
 @app.route("/teacher/COSC310/assignments")
 def teacherCourseAssignments():
     return render_template("teacher/assignmentsTab.html")
+
+@app.route("/teacher/COSC310/publishQuiz", methods = ['POST', 'GET'])
+def publishQuiz():
+    questionForm = request.form
+    return render_template("teacher/publishQuiz.html", questionForm=questionForm)
 
 if __name__ == "__main__":
     app.run()
