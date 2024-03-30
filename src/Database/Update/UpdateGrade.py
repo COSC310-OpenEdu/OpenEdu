94a86054b64e575cedea1f3a131f2ff4f53f2755
SilverSix6 <aidanmorris810@gmail.com>
2024-03-30 12:21:02 -0700

    Added comments to UpdateGrade.py

--- src/Database/Update/UpdateGrade.py
+++ src/Database/Update/UpdateGrade.py
@@ -3,6 +3,13 @@ from src.Database.DatabaseManager import DatabaseManager
 import mysql
 
 class UpdateGrade(DatabaseUpdate):
+    
+    #
+    #   Updates the grade of a question on an assignment
+    #   Input:  dataTuple = (grade, courseId, questionId, assignmentId, studentId)
+    #   Output: N/A
+    #
+    
     @classmethod 
     def update(cls, dataTuple):
         updateGrade = "UPDATE Grades SET grade=%s WHERE courseId=%s and questionId=%s and assignmentId = %s and studentId=%s"
