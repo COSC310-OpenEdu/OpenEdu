76a04aac97ec4d60e2c051ded0f42e948f339a63
SilverSix6 <aidanmorris810@gmail.com>
2024-03-30 09:26:48 -0700

    Added tests for adding course requests

---
+++ tests/Database/Update/test_AddCourseRequest.py
@@ -0,0 +1,22 @@
+from src.Database.Update.AddCourseRequest import AddCourseRequest
+from src.Database.Check.CheckCourseRequest import CheckCourseRequest
+from tests.Database.DatabaseTestManager import DatabaseTestManager
+import pytest
+
+
+def test_AddCourseRequest():
+    AddCourseRequest.update((1,3))
+    
+    assert(CheckCourseRequest.check((1,3)))
+    
+    DatabaseTestManager.rollback()
+    
+    
+def test_AddCourseRequest_Multiple():
+    AddCourseRequest.update((1,3))
+    AddCourseRequest.update((2,3))
+    
+    assert(CheckCourseRequest.check((1,3)))
+    assert(CheckCourseRequest.check((2,3)))
+    
+    DatabaseTestManager.rollback()
