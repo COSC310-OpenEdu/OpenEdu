204d3a3203f1f86a988972ad0e151753d2404e94
SilverSix6 <aidanmorris810@gmail.com>
2024-03-30 10:26:20 -0700

    Added testing for create account. Tests both students and instructors

---
+++ tests/Database/Update/test_CreateAccount.py
@@ -0,0 +1,32 @@
+from src.Database.Update.CreateAccount import CreateAccount
+from tests.Database.DatabaseTestManager import DatabaseTestManager
+from src.Database.Query.GetPrimaryKeyOfLastInsert import GetPrimaryKeyOfLastInsert
+from src.Database.Check.CheckUser import CheckUser
+from src.Database.Check.CheckStudent import CheckStudent
+from src.Database.Check.CheckInstructor import CheckInstuctor
+import pytest
+
+def test_CreateAccount_student():
+    CreateAccount.update(('student', 'fname', 'lname', 'email', 'password', 'username'))
+    
+    primaryKey = GetPrimaryKeyOfLastInsert.query(None)
+
+    assert(CheckUser.check((primaryKey[0],)))
+    assert(CheckStudent.check((primaryKey[0],)))
+    
+    DatabaseTestManager.rollback()
+    DatabaseTestManager.rollback()
+    
+    
+def test_CreateAccount_Instructor():
+    CreateAccount.update(('instructor', 'fname', 'lname', 'email', 'password', 'username'))
+    
+    primaryKey = GetPrimaryKeyOfLastInsert.query(None)
+
+    assert(CheckUser.check((primaryKey[0],)))
+    assert(CheckInstuctor.check((primaryKey[0],)))
+    
+    DatabaseTestManager.rollback()
+    DatabaseTestManager.rollback()
+    
+    
