6873e579d787f1d1a1d2b4cc1586892d6757e420
SilverSix6 <aidanmorris810@gmail.com>
2024-03-31 09:18:11 -0700

    Added comments for SelectGradesForAssignment.py

--- src/Database/Query/SelectGradesForAssignment.py
+++ src/Database/Query/SelectGradesForAssignment.py
@@ -4,12 +4,17 @@ import mysql
 
 
 class SelectGradesForAssignment(DatabaseQueryAll):
+    
+    #
+    #   Selects the all questions grades and comments for a given courseId, assignmentId, and studentId
+    #   Input:  dataTuple = (courseId, assignmentId, studentId)
+    #   Ouput:  A list of questions and grades for the given assignment
+    #
+    
     @classmethod
     def queryAll(cls, dataTuple) -> tuple:
         getGrades = "SELECT questionId, grade, comment FROM Grades WHERE courseId=%s and assignmentId=%s and studentId=%s"
 
-
-        
         cursor = DatabaseManager.getDatabaseCursor();
         cursor.execute(getGrades, (dataTuple))
         allGrades = cursor.fetchall();
