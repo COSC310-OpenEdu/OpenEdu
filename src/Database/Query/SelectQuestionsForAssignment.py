cee63c4fac8a3e8fcc6da3f51e787412a614a5a6
SilverSix6 <aidanmorris810@gmail.com>
2024-03-31 09:20:55 -0700

    Added comment for SelectQuestionsForAssignment.py

--- src/Database/Query/SelectQuestionsForAssignment.py
+++ src/Database/Query/SelectQuestionsForAssignment.py
@@ -3,8 +3,15 @@ from src.Database.DatabaseManager import DatabaseManager;
 import mysql
 
 class SelectQuestionsForAssignment(DatabaseQueryAll):
+    
+    #
+    #   Retrieves question information for an assignment 
+    #   Input:  dataTuple = (courseId, assignmentId)
+    #   Output: A list of questions and its information
+    #
+    
     @classmethod
-    def queryAll(cls, dataTuple) -> tuple:
+    def queryAll(cls, dataTuple) -> list:
        
         cursor = DatabaseManager.getDatabaseCursor()
         sql = """SELECT questionId, questionText, assignmentId, questionAnswer, longQuestion FROM Question
