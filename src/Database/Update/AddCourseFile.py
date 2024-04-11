de5acaae29555d0f05c681101d7258a3e545bf05
SilverSix6 <aidanmorris810@gmail.com>
2024-04-10 17:08:09 -0700

    Added comments to the database updates

--- src/Database/Update/AddCourseFile.py
+++ src/Database/Update/AddCourseFile.py
@@ -2,6 +2,14 @@ from src.Database.DatabaseManager import DatabaseManager
 from src.Database.Query.DatabaseQuery import DatabaseQuery
 
 class AddCourseFile(DatabaseQuery):
+    
+    #
+    #   Inserts information about a file and where it is stored
+    #   Input: courseId = the course this file is stored in
+    #          file_path = The path the file is store in the file system
+    #          file_name = The name of the file in the file system
+    #   Output: N/A
+    
     @classmethod
     def insert_file_record(cls, courseId, file_path, file_name):
         query = "INSERT INTO CourseFiles (courseId, fileLocator, fileName) VALUES (%s, %s, %s)"
